623e29b3251a3aded7e310bd7a47be694c2f1311
victorHugoAguilar <victorhugoaguilar@aol.com>
2020-08-01 04:02:40 +0200

    Añandir localStorage para añadir citas en el local

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 // Importar el componente personalizado
 import Formulario from './Components/Formulario';
 import Cita from './Components/Cita';
 
 function App() {
+
+  // Citas en local storage
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+  if(!citasIniciales){
+    citasIniciales = [];
+  }
+
   // Array de todas las citas creadas
-  const [ citas, setCitas] = useState([]);
+  const [ citas, setCitas] = useState(citasIniciales);
+
+  // use useEffect para realizar ciertas operaciones cuando el state cambia
+  useEffect( () => {
+    if(citasIniciales){
+      localStorage.setItem('citas', JSON.stringify(citas));
+    }else{
+      localStorage.setItem('citas', JSON.stringify([]));
+    }
+  }, [citas] );
 
   // Función que elimina una cita
   const deleteCita = (id) => {
@@ -14,7 +30,6 @@ function App() {
     const nuevasCitas = citas.filter(cita => cita.id !== id );
     setCitas(nuevasCitas);
   }
-
   // Función que añade nuevas citas al array
   const addNewCita = (cita) => {
     console.log('ADD_NEW_CITA');
@@ -38,9 +53,7 @@ function App() {
             <Formulario addNewCita={addNewCita}/>
           </div> 
           <div className="one-half column">
-        
           <h2>{titulo}</h2>
-
             {
               citas.map( cita => (
                 <Cita 
